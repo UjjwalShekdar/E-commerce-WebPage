065a2edb808818386c63315ae963721d79b63abc
ujjwal shekdar <ujjwalshekdar@gmail.com>
2023-08-28 18:02:11 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            product: "Ujjwal Shekdar",
+            des: "Building new project with vue",
+            sign: "US"
+        }
+    }
+})
