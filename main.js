2c4b3181d70d2d488a2d5070bfb79cc7addc065b
ujjwal shekdar <ujjwalshekdar@gmail.com>
2023-08-28 18:36:01 +0530

    second commit

--- main.js
+++ main.js
@@ -3,7 +3,15 @@ const app = Vue.createApp({
         return {
             product: "Ujjwal Shekdar",
             des: "Building new project with vue",
-            sign: "US"
+            image: './assets/images/socks_green.jpg',
+            inventory: 100,
+            onSale: false,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 1, color: 'green'},
+                {id: 2, color: 'blue'}
+            ],
+            sizes: ['S', 'M', 'L', 'XL'],
         }
     }
 })
