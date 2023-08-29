900f28c761de956177b9a5644699cf91c26a4e94
ujjwal shekdar <ujjwalshekdar@gmail.com>
2023-08-29 16:03:01 +0530

    third commit

--- main.js
+++ main.js
@@ -2,15 +2,16 @@ const app = Vue.createApp({
     data(){
         return {
             cart: 0,
-            product: "Ujjwal Shekdar",
+            product: "Socks",
             des: "Building new project with vue",
             image: './assets/images/socks_green.jpg',
             inventory: 100,
             onSale: false,
+            inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                {id: 1, color: 'green'},
-                {id: 2, color: 'blue'}
+                {id: 1, color: 'green', image: './assets/images/socks_green.jpg'},
+                {id: 2, color: 'blue', image: './assets/images/socks_blue.jpg'}
             ],
             sizes: ['S', 'M', 'L', 'XL'],
 
@@ -19,6 +20,14 @@ const app = Vue.createApp({
     methods: {
         addToCart(){
             this.cart += 1
+        },
+        removeFromCart() {
+            if (this.cart > 0) {
+              this.cart-=1;
+            }
+          },
+        updateImage(variantImage){
+            this.image = variantImage
         }
     }
 })
