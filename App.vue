34595699352c04dc6c5e320188f71a437701ce55
ujjwal shekdar <ujjwalshekdar@gmail.com>
2023-08-31 12:16:49 +0530

    6th commit

---
+++ App.vue
@@ -0,0 +1,36 @@
+// App.vue
+<template>
+  <div>
+    <h1>Todos</h1>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.title }}
+      </li>
+    </ul>
+    <p>Completed Todos: {{ completedTodoCount }}</p>
+    <p>Total Todos: {{ totalTodoCount }}</p>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters, mapActions } from 'vuex';
+
+export default {
+  computed: {
+    ...mapState(['todos']),
+    ...mapGetters(['completedTodos', 'todoCount']),
+    completedTodoCount() {
+      return this.completedTodos.length;
+    },
+    totalTodoCount() {
+      return this.todoCount;
+    },
+  },
+  created() {
+    this.fetchTodos();
+  },
+  methods: {
+    ...mapActions(['fetchTodos']),
+  },
+};
+</script>
